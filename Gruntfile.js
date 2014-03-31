e52826557b7fed5cf1f3962a98fbdd1e3f2df5ff
Pablo Villoslada Puigcerber <pablo85@gmail.com>
2014-03-31 11:58:43 +0200

    Add UglifyJS task to minify files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,63 +2,70 @@ module.exports = function (grunt) {
 	'use strict';
     // Project configuration
     grunt.initConfig({
-        // Task configuration
-        copy: {
-            main: {
-                expand: true,
-                cwd: 'lib/',
-                src: ['capitalize.js'],
-                dest: './'
-            }
-        },
-        jshint: {
-            options: {
-                node: true,
-                curly: true,
-                eqeqeq: true,
-                immed: true,
-                latedef: true,
-                newcap: true,
-                noarg: true,
-                sub: true,
-                undef: true,
-                unused: true,
-                boss: true,
-                eqnull: true,
-                globals: {
-                  "angular"    : false,
-                  "describe"   : false,
-                  "it"         : false,
-                  "before"     : false,
-                  "beforeEach" : false,
-                  "after"      : false,
-                  "afterEach"  : false,
-                  "inject"  : false,
-                  "expect"  : false
-                }
-            },
-            gruntfile: {
-                src: 'Gruntfile.js'
-            },
-            lib_test: {
-                src: ['lib/**/*.js', 'test/**/*.js']
-            }
-        },
-        karma: {
-          unit: {
-            configFile: 'karma.conf.js'
+      // Task configuration
+      copy: {
+        main: {
+          expand: true,
+          cwd: 'lib/',
+          src: ['capitalize.js'],
+          dest: './'
+        }
+      },
+      jshint: {
+        options: {
+          node: true,
+          curly: true,
+          eqeqeq: true,
+          immed: true,
+          latedef: true,
+          newcap: true,
+          noarg: true,
+          sub: true,
+          undef: true,
+          unused: true,
+          boss: true,
+          eqnull: true,
+          globals: {
+            "angular"    : false,
+            "describe"   : false,
+            "it"         : false,
+            "before"     : false,
+            "beforeEach" : false,
+            "after"      : false,
+            "afterEach"  : false,
+            "inject"  : false,
+            "expect"  : false
           }
         },
-        watch: {
-            gruntfile: {
-                files: '<%= jshint.gruntfile.src %>',
-                tasks: ['jshint:gruntfile']
-            },
-            lib_test: {
-                files: '<%= jshint.lib_test.src %>',
-                tasks: ['jshint:lib_test', 'karma']
-            }
+        gruntfile: {
+          src: 'Gruntfile.js'
+        },
+        lib_test: {
+          src: ['lib/**/*.js', 'test/**/*.js']
         }
+      },
+      karma: {
+        unit: {
+          configFile: 'karma.conf.js'
+        }
+      },
+      uglify: {
+        my_target: {
+          files: {
+            './capitalize.min.js': ['lib/capitalize.js']
+          }
+        }
+      },
+      watch: {
+        gruntfile: {
+          files: '<%= jshint.gruntfile.src %>',
+          tasks: ['jshint:gruntfile']
+        },
+        lib_test: {
+          files: '<%= jshint.lib_test.src %>',
+          tasks: ['jshint:lib_test', 'karma']
+        }
+      }
     });
 
     // These plugins provide necessary tasks
@@ -66,7 +73,10 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // Default task
-    grunt.registerTask('default', ['jshint', 'karma', 'copy']);
+    grunt.registerTask('default', ['jshint', 'karma', 'build']);
+    // Build task
+    grunt.registerTask('build', ['copy', 'uglify']);
 };
--- capitalize.js
+++ capitalize.js
@@ -1,22 +1,26 @@
 'use strict';
-
 /**
  * Capitalize Filter
  * Capitalizes all the words of a given sentence.
- * Specially adapted for team names.
+ * If the format parameter is set to 'team', uppercase the team abbreviation.
  * i.e. CLUB DEPORTIVO LOGROÑÉS => Club Deportivo Logroñés
  * i.e. sd logroñés => SD Logroñés
+ * @author Pablo Villoslada Puigcerber <pablo85@gmail.com>
+ *
+ * @param {string} input The string to be formatted.
+ * @param {string} [format] The format to be applied being the options 'all' or 'team'. If not specified, 'all' is used.
+ * @returns {string} Formatted string.
  */
-
 angular.module('customFilters',[])
   .filter('capitalize', function () {
-    return function (input) {
+    return function (input, format) {
+      format = format || 'all';
       if (typeof input !== 'undefined') {
         var words = input.split(' ');
         var result = [];
         words.forEach(function(word) {
-          if (word.length === 2) {
-            // For team abbreviations like FC, CD, SD
+          if (word.length === 2 && format === 'team') {
+            // Uppercase team abbreviations like FC, CD, SD
             result.push(word.toUpperCase());
           } else {
             result.push(word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
