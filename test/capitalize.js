99236e557afddb88d3d747dbda259832cba562ea
Pablo Villoslada Puigcerber <pablo85@gmail.com>
2014-03-31 11:22:24 +0200

    Add format parameter to the filter and test cases.

--- test/capitalize.js
+++ test/capitalize.js
@@ -11,14 +11,28 @@ describe('Filter: capitalize', function () {
     capitalize = $filter('capitalize');
   }));
 
-  it('should return the uppercase input with all the words capitalized', function () {
-    var text = 'CLUB DEPORTIVO LOGROÑÉS';
-    expect(capitalize(text)).toBe('Club Deportivo Logroñés');
+  describe('Format: team', function() {
+    it('should return the uppercase input with uppercase abbreviation and the rest of the words capitalized', function () {
+      var text = 'FC BARCELONA';
+      expect(capitalize(text, 'team')).toBe('FC Barcelona');
+    });
+
+    it('should return the lowercase input with uppercase abbreviation and the rest of the words capitalized', function () {
+      var text = 'sd logroñés';
+      expect(capitalize(text, 'team')).toBe('SD Logroñés');
+    });
   });
 
-  it('should return the lowercase input with all the words capitalized', function () {
-    var text = 'sd logroñés';
-    expect(capitalize(text)).toBe('SD Logroñés');
+  describe('Format: all', function() {
+    it('should return the uppercase input with all the words capitalized', function () {
+      var text = 'CLUB DEPORTIVO LOGROÑÉS';
+      expect(capitalize(text)).toBe('Club Deportivo Logroñés');
+    });
+
+    it('should return the lowercase input with all the words capitalized', function () {
+      var text = 'la tierra con nombre de vino';
+      expect(capitalize(text)).toBe('La Tierra Con Nombre De Vino');
+    });
   });
 
 });
